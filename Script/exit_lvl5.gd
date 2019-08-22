a39aaa91b564587b75738a9c0d93cb6e786425de
Lucas Z Fazzi <lzfazzi@gmail.com>
2019-08-21 22:04:18 -0300

    Add files via upload

---
+++ Script/exit_lvl5.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+func _ready():
+	pass
+
+
+func _on_exit_body_entered(body):
+	if body.is_in_group("player"):
+		if $exit.is_playing():
+			pass
+		if not $exit.is_playing():
+			$exit.play()
+		var waiting_timer = Timer.new()
+		waiting_timer.set_wait_time(0.3)
+		waiting_timer.set_one_shot(true)
+		self.add_child(waiting_timer)
+		waiting_timer.start()
+		yield(waiting_timer, "timeout")
+		get_tree().call_deferred("change_scene","res://Scenes/lvl6.tscn")
+		call_deferred("queue_free")
+	else:
+		pass
